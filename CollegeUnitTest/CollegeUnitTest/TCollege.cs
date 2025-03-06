4689f0f2dcbc306a1378c733b2169262fcc5361b
uxq <tcvegor2006@mail.ru>
2025-03-06 13:02:03 +0300

    добавил тест

---
+++ CollegeUnitTest/CollegeUnitTest/TCollege.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeUnitTest
+{
+    public class TCollege
+    {
+        public Dictionary<string, List<string>> expectedInfo = new Dictionary<string, List<string>>()
+        {
+            {"П-10", new List<string>() { "Баранов Даниил" , "Курчаков Артем", "Колобанов Захар"} },
+            {"Л-20", new List<string>() { "Смирнова Екатерина" , "Бычков Дмитрий", "Суханов Дмитрий"} },
+            {"Н-30", new List<string>() { "Венедиктов Дмитрий" , "Сучков Артем", "Водянов Захар"} },
+        };
+
+        [TestMethod]
+        public void TGetStudentsByGroupX()
+        {
+            College college = new College();
+            foreach (string group in expectedInfo.Keys)
+            {
+                college.AddGroup(group, expectedInfo[group]);
+
+                List<string> actual = college.GetStudentsByGroup(group);
+
+                List<string> expected = new List<string>(expectedInfo[group]);
+
+                CollectionAssert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
---
+++ CollegeUnitTest/MyLib/College.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class College
+    {
+        public Dictionary<string, List<string>> Students = new Dictionary<string, List<string>>();
+        public void AddGroup(string group, List<string> Student)
+        {
+            Students.Add(group, Student);
+        }
+
+        public List<string> GetStudentsByGroup(string group)
+        {
+            return new List<string>() { };
+        }
+    }
+}
