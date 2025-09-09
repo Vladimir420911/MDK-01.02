f957a5a23953a8bb7e419e274ded03dd0baddf73
Kate <20katerina0060@gmail.com>
2025-09-09 13:09:41 +0300

    Добавил пустой проект ms test

---
+++ mdk01.02/UnitTestProjectMoq/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectMoq")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectMoq")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6601609-1b73-4590-ba33-f58ba4d1892e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mdk01.02/UnitTestProjectMoq/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestProjectMoq
+{
+    [TestClass]
+    public class MoqTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
